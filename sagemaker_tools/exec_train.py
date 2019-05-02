367ce6b163792c265ef21f73466ba1448da515ca
tn1031 <ttt.nakamura1031@gmail.com>
2019-05-02 16:09:41 +0900

    hp tuner

--- sagemaker_tools/exec_train.py
+++ sagemaker_tools/exec_train.py
@@ -8,9 +8,13 @@ import sagemaker
 from sagemaker import get_execution_role
 from sagemaker.chainer.estimator import Chainer
 from sagemaker.pytorch.estimator import PyTorch
+from sagemaker.tuner import HyperparameterTuner, CategoricalParameter, \
+                            ContinuousParameter, IntegerParameter
+hp_type = {'continuous': ContinuousParameter,
+           'integer': IntegerParameter,
+           'categorical': CategoricalParameter}
 
-
-def exec_training(session, client, job_name, setting, pytorch):
+def exec_training(session, client, job_name, setting, pytorch, max_parallel_jobs):
     sagemaker_session = sagemaker.Session(
         boto_session=session,
         sagemaker_client=client)
@@ -29,12 +33,52 @@ def exec_training(session, client, job_name, setting, pytorch):
 
     estimator_args = conf['estimator']
     estimator_args['sagemaker_session'] = sagemaker_session
+
+    hyperparameters = estimator_args.pop('hyperparameters')
+    fixed, targets = {}, {}
+    for k, v in hyperparameters.items():
+        if isinstance(v, dict):
+            targets[k] = v
+        else:
+            fixed[k] = v
+    estimator_args['hyperparameters'] = fixed
+
     if pytorch:
         estimator = PyTorch(**estimator_args)
     else:
         estimator = Chainer(**estimator_args)
 
-    estimator.fit(inputs, job_name=job_name)
+    if len(targets) == 0:
+        estimator.fit(inputs, job_name=job_name)
+    else:
+        if 'tuner' in conf:
+            tuner_args = conf['tuner']
+            hyperparameter_ranges = {}
+            for k, v in targets.items():
+                hyperparameter_ranges[k] = hp_type[v['type'].lower()](v['range'])
+        else:  # use default values
+            tuner_args = {'objective_metric_name': 'metric_name',
+                          'metric_definitions': [{'Name': 'metric_name', 'Regex': 'ignore'}],
+                          'strategy': 'Random',
+                          'objective_type': 'Maximize',
+                          'early_stopping_type': 'Off'}
+            max_jobs = 1
+            hyperparameter_ranges = {}
+            for k, v in targets.items():
+                if v['type'].lower() != 'categorical':
+                    raise ValueError('the default tuner only supports Categorigal params.')
+                max_jobs *= len(v['range'])
+                hyperparameter_ranges[k] = hp_type[v['type'].lower()](v['range'])
+            tuner_args['max_jobs'] = max_jobs
+
+        tuner_args['estimator'] = estimator
+        tuner_args['hyperparameter_ranges'] = hyperparameter_ranges
+        tuner_args['max_parallel_jobs'] = max_parallel_jobs
+        tuner_args['base_tuning_job_name'] = job_name
+        tuner_args['warm_start_config'] = None  # not supported yet.
+
+        tuner = HyperparameterTuner(**tuner_args)
+        tuner.fit(inputs)
 
 
 def main():
@@ -46,6 +90,8 @@ def main():
     parser.add_argument('--profile_name', '-p', type=str, default=None,
                         help='When execute a training from local, enter the profile name.')
     parser.add_argument('--pytorch', '-t', action='store_true')
+    parser.add_argument('--max_parallel_jobs', type=int, default=1,
+                        help='# wokers for bulk training.')
     args = parser.parse_args()
 
     if args.profile_name is None:
@@ -60,4 +106,5 @@ def main():
                               aws_secret_access_key=credentials.secret_key,
                               aws_session_token=credentials.token)
 
-    exec_training(session, client, args.job_name, args.setting, args.pytorch)
+    exec_training(session, client, args.job_name, args.setting, args.pytorch,
+                  args.max_parallel_jobs)
