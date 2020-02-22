ece41e1b4f53195c4c8ef071ce49395d3f929f70
tn1031 <ttt.nakamura1031@gmail.com>
2020-02-22 17:51:11 +0900

    spot training option

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
 from setuptools import setup
 
 setup(
-    author='tn1031',
-    author_email='ttt.nakamura1031@gmail.com',
-    name='chainer_sagemaker_tools',
-    description='Some extensions and tools to run Chainer jobs on Amazon SageMaker',
-    version='0.1.8.4',
-    packages=['sagemaker_tools', 'sage_extensions'],
-    install_requires=[
-        'boto3',
-        'pyyaml>=4.2b1',
-        'sagemaker',
-        'slackweb'],
+    author="tn1031",
+    author_email="ttt.nakamura1031@gmail.com",
+    name="chainer_sagemaker_tools",
+    description="Some extensions and tools to run Chainer jobs on Amazon SageMaker",
+    version="0.1.8.5",
+    packages=["sagemaker_tools", "sage_extensions"],
+    install_requires=["boto3", "pyyaml>=4.2b1", "sagemaker", "slackweb"],
     entry_points={
-        'console_scripts': ['smtrain=sagemaker_tools.exec_train:main',
-                            'smdeploy=sagemaker_tools.deploy_endpoint:main',
-                            'smbatch=sagemaker_tools.batch_inference:main'],
+        "console_scripts": [
+            "smtrain=sagemaker_tools.exec_train:main",
+            "smdeploy=sagemaker_tools.deploy_endpoint:main",
+            "smbatch=sagemaker_tools.batch_inference:main",
+        ]
     },
-    license='MIT license',
+    license="MIT license",
 )
